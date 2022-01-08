82451acd447a916849faff5150b91e2454c4f48f
Johdasgran <dampig14@gmail.com>
2022-01-08 12:14:14 -0500

    add desigs js

---
+++ e.js
@@ -0,0 +1,19 @@
+// > whoami
+// Johan Grandas
+
+// Frontend developer
+
+const aboutme = {
+  //I'm Johan Gandas, live in
+  //Bucaramanga, i like the design and
+  //programming.
+};
+
+const projects = {
+    "batatabit": "🥔 La próxima revolución en el intercambio de criptomonedas",
+    "best-food": "🥗 Check the best food",
+    "social70plus": "👴 Como expertos en la tecnologia",
+    "yard-sale": "App de ventas de productos",
+    "aprende-de-cocina": "Blog de recetas de cocina",
+    "ColCeres": "El marketplace online más completo para tus productos"
+};
